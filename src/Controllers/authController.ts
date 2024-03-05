547265ae340c8d78fcc15ec627a3d018c87a1bd2
Edgar Tolete <edgartolete@outlook.com>
2024-03-06 01:02:14 +0800

    refresh token feature added.

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -5,7 +5,7 @@ import { fakeDelay, generateAccessToken, generateId, tryCatch, tryCatchAsync } f
 import { Log } from '../Connections/mongoDB';
 import { getRuntimeConfig } from '../config';
 import { UserCreateType, UserFindType, userModel } from '../Models/usersModel';
-import jwt, { Secret } from 'jsonwebtoken';
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
 import { redisClient } from '../Connections/redis';
 
 const { encryptionKey } = getRuntimeConfig();
@@ -106,7 +106,7 @@ export const authController = {
 		if (accessToken === null) return JsonResponse.failed1(res, null, 'Generating Access Token returned null');
 
 		const [refreshToken, rtErr] = tryCatch(() => {
-			return jwt.sign({ id: result.id, username, email }, das, { expiresIn: '10d' });
+			return jwt.sign({ id: result.id, username, email }, drs, { expiresIn: '10d' });
 		});
 
 		if (rtErr !== null) return JsonResponse.error(res, rtErr);
@@ -126,19 +126,47 @@ export const authController = {
 		// this should be dynamic based on app if they want to enable.
 	},
 	refresh: async (req: Request, res: Response) => {
-		try {
-			const refreshTokenSecret = req.headers['refresh-token-secret'];
+		const apiKey = req.headers['x-api-key'] as string;
+		const accessTokenSecret = (req.headers['access-token-secret'] as string) ?? '';
+		const refreshTokenSecret = (req.headers['refresh-token-secret'] as string) ?? '';
 
-			await fakeDelay(3000);
-			JsonResponse.success(res);
+		const { refreshToken = null } = req.body;
 
-			return;
-		} catch (err) {
-			JsonResponse.failed(res, err);
+		if (refreshToken == null) {
+			return JsonResponse.incompleteData(res, null, 'Required refreshToken in body.');
 		}
+
+		const [das, dasErr] = tryCatch(() => secure.decrypt(accessTokenSecret, apiKey));
+		if (dasErr !== null) return JsonResponse.error(res, dasErr);
+		if (das == null) return JsonResponse.failed1(res, null, 'Access Token Secret Decrypt Failed.');
+
+		const [drs, drsErr] = tryCatch(() => secure.decrypt(refreshTokenSecret, apiKey));
+		if (drsErr !== null) return JsonResponse.error(res, drsErr);
+		if (drs == null) return JsonResponse.failed1(res, null, 'Refresh Token Secret Decrypt Failed.');
+
+		type RefreshTokenType = {
+			id: string;
+			username: string;
+			email?: string;
+			iat: Date;
+			exp: Date;
+		};
+		const [dt, dtErr] = tryCatch(() => jwt.verify(refreshToken, drs as Secret));
+
+		if (dtErr !== null) return JsonResponse.error(res, dtErr);
+
+		const { iat, exp, ...rest } = dt as JwtPayload;
+
+		const [accessToken, atErr] = tryCatch(() => {
+			return jwt.sign(rest, das, { expiresIn: '10m' });
+		});
+		if (atErr !== null) return JsonResponse.error(res, atErr);
+		if (accessToken === null) return JsonResponse.failed1(res, null, 'Generating Access Token returned null');
+
+		return JsonResponse.success(res, { accessToken }, 'Refresh Token Success');
 	},
 	logout: async (req: Request, res: Response) => {
-		const id = req.headers['user-id']?.toString();
+		const id = req.headers['user-id']?.toString() ?? '';
 
 		const [redisAt, redisRt] = await Promise.all([
 			redisClient.del(`${id}-access-token`),
@@ -151,13 +179,7 @@ export const authController = {
 
 		return JsonResponse.success(res, null, 'You are now successfully logged-out');
 	},
-
 	forgot: async (req: Request, res: Response) => {
-		try {
-			JsonResponse.success(res);
-			return;
-		} catch (err) {
-			JsonResponse.failed(res, err);
-		}
+		//
 	}
 };
